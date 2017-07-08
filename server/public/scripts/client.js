9a81c13a80b0ee4c70bc6bc8e256f1576f01e1dc
bendrumin <bendrumin@mac.com>
2017-07-08 09:14:19 -0500

    Delete function works, add does not

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,37 @@ console.log('JS Sourced');
 $(document).ready(function() {
   console.log('js');
   getTasks();
+  // add task button click
+  $( '#submitBtn' ).on( 'click', function(){
+    console.log( 'in addButton on click' );
+    var newTask = {}
+    task.item = $('#newTask').val();
+    console.log(newTask);
+    // call saveKoala with the new obejct
+    saveTask( newTask );
+  });
+  //Task complete function
+  $('#itemList').on('click', '.completeBtn' , function(){
+    console.log('Delete task as been hit');
+    var itemid = $(this).data('itemid');
+    console.log($(this));
+    console.log('Delete koala with id of', itemid);
+    completeTask(itemid);
+  });
 });
+function saveTask( newTask ){
+  console.log( 'in saveTask', newTask );
+  // ajax call to server to get tasks
+  $.ajax({
+    url: '/tasks',
+    type: 'POST',
+    data: newTask,
+    success: function( data ){
+      console.log( 'got some tasks: ', data );
+      refreshTasks();
+    } // end success
+  }); //end ajax
+}
 function getTasks(){
   console.log( 'in get tasks' );
   // ajax call to server to get
@@ -17,20 +47,20 @@ function getTasks(){
   }); //end ajax
   // display on DOM with buttons that allow edit of each
 } // end
-function saveTask( newTask ){
-  console.log( 'in saveKoala', newTask );
-  // ajax call to server to get tasks
+function refreshTasks() {
   $.ajax({
+    type: 'GET',
     url: '/tasks',
-    type: 'POST',
-    data: newTask,
-    success: function( data ){
-      console.log( 'got some tasks: ', data );
-    } // end success
-  }); //end ajax
+    success: function(response) {
+      console.log(response);
+      appendToDom(response.tasklist);
+    }
+  });
+
 }
+
 function appendToDom(tasks) {
-  console.log('Koala Append Working');
+  console.log('List Append Working');
   // Remove tasks that currently exist in the table
   $('#itemList').empty();
   for(var i = 0; i < tasks.length; i += 1) {
@@ -40,7 +70,19 @@ function appendToDom(tasks) {
     $tr.data('task', task);
     $tr.append('<td>' + task.item + '</td>');
 
-    $tr.append('<td><button class="completeBtn" data-taskid="' + task.id + '">Complete</button></td>');
+    $tr.append('<td><button class="completeBtn" data-itemid="' + task.id + '">Complete</button></td>');
     $('#itemList').append($tr);
   }
+}
+function completeTask(itemid) {
+  console.log(itemid);
+  $.ajax({
+    type: 'DELETE',
+    url: '/tasks/' + itemid,
+    success: function(response) {
+      console.log(response);
+      console.log('I deleted the koalatask');
+
+    }
+  });
 }
